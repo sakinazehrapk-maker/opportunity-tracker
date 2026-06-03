bd8778f815b6c1a68317070da71cab7a98c039dd
sakina <sakinazehrapk@gmail.com>
2026-06-03 22:45:37 +0500

    basic setup

---
+++ script.js
@@ -0,0 +1,34 @@
+const form=document.getElementById("opportunityForm");
+const opportunityList=document.getElementById("opportunityList");
+
+let opportunities=[];
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const opportunity={
+        title:document.getElementById("title").value,
+        category:document.getElementById("category").value,
+        organization:document.getElementById("organization").value,
+        link:document.getElementById("link").value,
+        notes:document.getElementById("notes").value
+    };
+    opportunities.push(opportunity);
+
+    displayOpportunities();
+    form.reset();
+});
+function displayOpportunities(){
+    opportunityList.innerHTML="";
+    opportunities.forEach(opportunity=> {
+        const card=document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML =`
+            <h3>${opportunity.title}</h3>
+            <p><strong>Category:</strong> ${opportunity.category}</p>
+            <p><strong>Organization:</strong> ${opportunity.organization}</p>
+            <p><strong>Deadline:</strong> ${opportunity.deadline}</p>
+            <p><a href="${opportunity.link}" target="_blank">Visit Link</a></p>
+            <p>${opportunity.notes}</p>
+        `;
+        opportunityList.appendChild(card);
+    });
+}
